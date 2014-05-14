fbf3d5ab1c7a9be2f53fc7e153047b29ddcdc27b
Anthony Grimes <i@raynes.me>
2014-05-14 07:27:05 -0700

    Create gh-pages if it isn't there.

--- src/leiningen/dox.clj
+++ src/leiningen/dox.clj
@@ -1,6 +1,7 @@
 (ns leiningen.dox
   (:require [me.raynes.conch :refer [programs]]
-            [me.raynes.fs :as fs]))
+            [me.raynes.fs :as fs]
+            [clojure.string :as s]))
 
 (programs git mv rm ls)
 
@@ -10,21 +11,33 @@
 (defn mv-contents [dir out-dir]
   (apply mv (conj (wildcard dir) out-dir)))
 
+(defn current-branch []
+  (s/trim-newline (git "rev-parse" "--abbrev-ref" "HEAD")))
+
+(defn ensure-gh-pages []
+  (if (zero? @(:exit-code
+               (git "show-ref" "--verify" "--quiet" "refs/heads/gh-pages"
+                    {:throw false :verbose true})))
+    (git "checkout" "gh-pages")
+    (do (println "*** gh-pages branch not found -- creating it ***")
+        (git "checkout" "--orphan" "gh-pages"))))
+
 (defn dox
   "Update documentation for the project."
   [project & args]
   (let [dir (fs/temp-dir "doxx")
         old-dir (or (get-in project [:dox :in])
                     (first args)
-                    "doc/")]
+                    "doc/")
+        branch (current-branch)]
     (mv-contents old-dir dir)
     (rm "-r" old-dir)
-    (git "checkout" "gh-pages")
+    (ensure-gh-pages)
     (git "rm" "-rf" ".")
     (mv-contents dir ".")
     (git "add" "-Av" ".")
     (git "commit" "-m" "Updated docs")
     (println "*** gh-pages branch updated ***")
-    (git "checkout" "-")
+    (git "checkout" branch)
     (println "Run this to complete:")
     (println "git push origin gh-pages:gh-pages")))
