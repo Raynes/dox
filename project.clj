fa11a6faa7fd22371bdd9f4a3d4c20b42843a98c
Anthony Grimes <i@raynes.me>
2014-05-14 06:44:07 -0700

    Should be me.raynes.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dox "0.1.0"
+(defproject me.raynes/dox "0.1.0"
   :description "A leiningen plugin to handle regenerating documentation."
   :url "https://github.com/Raynes/refheap/"
   :license {:name "Eclipse Public License"
